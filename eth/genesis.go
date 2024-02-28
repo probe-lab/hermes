2e0ae71bfa368b2cda056ab1acbb9880c0b028b4
Dennis Trautwein <git@dtrautwein.eu>
2024-02-28 12:10:35 +0100

    fix: genesis times

--- eth/genesis.go
+++ eth/genesis.go
@@ -35,19 +35,19 @@ func GetConfigsByNetworkName(net string) (*GenesisConfig, *params.NetworkConfig,
 var GenesisConfigs = map[string]*GenesisConfig{
 	params.MainnetName: {
 		GenesisValidatorRoot: hexToBytes("4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95"),
-		GenesisTime:          time.Unix(0, 1606824023*int64(time.Millisecond)),
+		GenesisTime:          time.Unix(1606824023, 0),
 	},
 	params.SepoliaName: {
 		GenesisValidatorRoot: hexToBytes("d8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078"),
-		GenesisTime:          time.Unix(0, 1655733600*int64(time.Millisecond)),
+		GenesisTime:          time.Unix(1655733600, 0),
 	},
 	params.PraterName: {
 		GenesisValidatorRoot: hexToBytes("043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb"),
-		GenesisTime:          time.Unix(0, 1616508000*int64(time.Millisecond)), // https://github.com/eth-clients/goerli
+		GenesisTime:          time.Unix(1616508000, 0), // https://github.com/eth-clients/goerli
 	},
 	params.HoleskyName: {
 		GenesisValidatorRoot: hexToBytes("9143aa7c615a7f7115e2b6aac319c03529df8242ae705fba9df39b79c59fa8b1"),
-		GenesisTime:          time.Unix(0, 1695902400*int64(time.Millisecond)),
+		GenesisTime:          time.Unix(1695902400, 0),
 	},
 }
 
