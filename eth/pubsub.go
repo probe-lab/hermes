82a722725d26dcef6ea3cea9033f35357607406d
Francis Li <francis.li@coinbase.com>
2024-05-02 16:46:46 -0700

    Add blob handler to capture blob p2p messages and produce event

--- eth/pubsub.go
+++ eth/pubsub.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	pubsubpb "github.com/libp2p/go-libp2p-pubsub/pb"
 	"github.com/libp2p/go-libp2p/core/peer"
@@ -108,6 +109,8 @@ func (p *PubSub) mapPubSubTopicWithHandlers(topic string) host.TopicHandler {
 	switch {
 	case strings.Contains(topic, p2p.GossipBlockMessage):
 		return p.handleBeaconBlock
+	case strings.Contains(topic, p2p.GossipBlobSidecarMessage):
+		return p.handleBlobSidecar
 	default:
 		return p.host.TracedTopicHandler(host.NoopHandler)
 	}
@@ -160,6 +163,50 @@ func (p *PubSub) handleBeaconBlock(ctx context.Context, msg *pubsub.Message) err
 
 	if err := p.cfg.DataStream.PutRecord(ctx, evt); err != nil {
 		slog.Warn("failed putting topic handler event", tele.LogAttrError(err))
+	}
+
+	return nil
+}
+
+func (p *PubSub) handleBlobSidecar(ctx context.Context, msg *pubsub.Message) error {
+	switch p.cfg.ForkVersion {
+	case DenebForkVersion:
+		var blob ethtypes.BlobSidecar
+		err := p.cfg.Encoder.DecodeGossip(msg.Data, &blob)
+		if err != nil {
+			slog.Error("decode blob sidecar gossip message", tele.LogAttrError(err))
+			return err
+		}
+
+		slot := blob.GetSignedBlockHeader().GetHeader().GetSlot()
+		slotStart := p.cfg.GenesisTime.Add(time.Duration(slot) * p.cfg.SecondsPerSlot)
+		proposerIndex := blob.GetSignedBlockHeader().GetHeader().GetProposerIndex()
+
+		now := time.Now()
+		evt := &host.TraceEvent{
+			Type:      "HANDLE_MESSAGE",
+			PeerID:    p.host.ID(),
+			Timestamp: now,
+			Payload: map[string]any{
+				"PeerID":     msg.ReceivedFrom.String(),
+				"MsgID":      hex.EncodeToString([]byte(msg.ID)),
+				"MsgSize":    len(msg.Data),
+				"Topic":      msg.GetTopic(),
+				"Seq":        msg.GetSeqno(),
+				"Slot":       slot,
+				"ValIdx":     proposerIndex,
+				"TimeInSlot": now.Sub(slotStart).Seconds(),
+				"StateRoot":  hexutil.Encode(blob.GetSignedBlockHeader().GetHeader().GetStateRoot()),
+				"BodyRoot":   hexutil.Encode(blob.GetSignedBlockHeader().GetHeader().GetBodyRoot()),
+				"ParentRoot": hexutil.Encode(blob.GetSignedBlockHeader().GetHeader().GetParentRoot()),
+			},
+		}
+
+		if err := p.cfg.DataStream.PutRecord(ctx, evt); err != nil {
+			slog.Warn("failed putting topic handler event", tele.LogAttrError(err))
+		}
+	default:
+		return fmt.Errorf("non recognized fork-version: %d", p.cfg.ForkVersion[:])
 	}
 
 	return nil
