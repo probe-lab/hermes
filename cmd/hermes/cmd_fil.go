71282caec99fb49e0e852a6e54e17a2539eb89a6
Dennis Trautwein <git@dtrautwein.eu>
2025-04-28 12:26:21 +0200

    filecoin: use configuration profiles

--- cmd/hermes/cmd_fil.go
+++ cmd/hermes/cmd_fil.go
@@ -20,14 +20,14 @@ var filConfig = &struct {
 	Libp2pHost                  string
 	Libp2pPort                  int
 	Libp2pPeerscoreSnapshotFreq time.Duration
-	BootrapperMaddrs            *cli.StringSlice
+	Network                     string
 	DialTimeout                 time.Duration
 }{
 	PrivateKeyStr:               "", // unset means it'll be generated
 	Libp2pHost:                  "127.0.0.1",
 	Libp2pPort:                  0,
 	Libp2pPeerscoreSnapshotFreq: 60 * time.Second,
-	BootrapperMaddrs:            cli.NewStringSlice(),
+	Network:                     "mainnet",
 	DialTimeout:                 5 * time.Second,
 }
 
@@ -37,7 +37,6 @@ var cmdFil = &cli.Command{
 	Usage:   "Listen to gossipsub topics of the Filecoin network",
 	Flags:   cmdFilFlags,
 	Action:  cmdFilAction,
-	Before:  cmdFilBefore,
 }
 
 var cmdFilFlags = []cli.Flag{
@@ -72,13 +71,12 @@ var cmdFilFlags = []cli.Flag{
 		Destination: &filConfig.Libp2pPort,
 		DefaultText: "random",
 	},
-	&cli.StringSliceFlag{
-		Name:        "bootstrappers",
-		EnvVars:     []string{"HERMES_FIL_BOOTSTRAPPERS"},
-		Usage:       "The peer multi addresses to use for connecting to the Filecoin network",
-		Value:       filConfig.BootrapperMaddrs,
-		Destination: filConfig.BootrapperMaddrs,
-		DefaultText: "mainnet",
+	&cli.StringFlag{
+		Name:        "network",
+		EnvVars:     []string{"HERMES_FIL_NETWORK"},
+		Usage:       "Which network hermes should connect to. Currently only 'mainnet' is supported.",
+		Value:       filConfig.Network,
+		Destination: &filConfig.Network,
 	},
 	&cli.DurationFlag{
 		Name:        "libp2p.peerscore.snapshot.frequency",
@@ -90,9 +88,17 @@ var cmdFilFlags = []cli.Flag{
 	},
 }
 
-func cmdFilBefore(c *cli.Context) error {
-	if !c.IsSet("bootstrappers") {
-		filConfig.BootrapperMaddrs = cli.NewStringSlice(
+func cmdFilAction(c *cli.Context) error {
+	slog.Info("Starting Hermes for Filecoin...")
+	defer slog.Info("Stopped Hermes for Filecoin.")
+
+	// Print hermes configuration for debugging purposes
+	printFilConfig()
+
+	var bootstrapperMaddrStrs []string
+	switch filConfig.Network {
+	case "mainnet":
+		bootstrapperMaddrStrs = []string{
 			"/dns/node.glif.io/tcp/1235/p2p/12D3KooWBF8cpp65hp2u9LK5mh19x67ftAam84z9LsfaquTDSBpt",
 			"/dns/bootstrap-venus.mainnet.filincubator.com/tcp/8888/p2p/QmQu8C6deXwKvJP2D8B6QGyhngc3ZiDnFzEHBDx8yeBXST",
 			"/dns/bootstrap-mainnet-0.chainsafe-fil.io/tcp/34000/p2p/12D3KooWKKkCZbcigsWTEu1cgNetNbZJqeNtysRtFpq7DTqw3eqH",
@@ -101,21 +107,13 @@ func cmdFilBefore(c *cli.Context) error {
 			"/dns/n1.mainnet.fil.devtty.eu/udp/443/quic-v1/p2p/12D3KooWAke3M2ji7tGNKx3BQkTHCyxVhtV1CN68z6Fkrpmfr37F",
 			"/dns/n1.mainnet.fil.devtty.eu/tcp/443/p2p/12D3KooWAke3M2ji7tGNKx3BQkTHCyxVhtV1CN68z6Fkrpmfr37F",
 			"/dns/n1.mainnet.fil.devtty.eu/udp/443/quic-v1/webtransport/certhash/uEiAWlgd8EqbNhYLv86OdRvXHMosaUWFFDbhgGZgCkcmKnQ/certhash/uEiAvtq6tvZOZf_sIuityDDTyAXDJPfXSRRDK2xy9UVPsqA/p2p/12D3KooWAke3M2ji7tGNKx3BQkTHCyxVhtV1CN68z6Fkrpmfr37F",
-		)
+		}
+	default:
+		return fmt.Errorf("unknown network: %s", filConfig.Network)
 	}
 
-	return nil
-}
-
-func cmdFilAction(c *cli.Context) error {
-	slog.Info("Starting Hermes for Filecoin...")
-	defer slog.Info("Stopped Hermes for Filecoin.")
-
-	// Print hermes configuration for debugging purposes
-	printFilConfig()
-
-	bootstrappers := make([]peer.AddrInfo, len(filConfig.BootrapperMaddrs.Value()))
-	for i, maddrStr := range filConfig.BootrapperMaddrs.Value() {
+	bootstrappers := make([]peer.AddrInfo, len(bootstrapperMaddrStrs))
+	for i, maddrStr := range bootstrapperMaddrStrs {
 		bp, err := peer.AddrInfoFromString(maddrStr)
 		if err != nil {
 			slog.Warn("Failed parsing bootstrapper multiaddress", slog.String("maddr", maddrStr), tele.LogAttrError(err))
@@ -136,7 +134,6 @@ func cmdFilAction(c *cli.Context) error {
 		S3Config:                    rootConfig.s3Config,
 		KinesisRegion:               rootConfig.KinesisRegion,
 		KinesisStream:               rootConfig.KinesisStream,
-		DialConcurrency:             filConfig.DialConcurrency,
 		Tracer:                      otel.GetTracerProvider().Tracer("hermes"),
 		Meter:                       otel.GetMeterProvider().Meter("hermes"),
 	}
