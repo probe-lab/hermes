6886fbfe6b63fbe2a986676cf2c7320f9b1fecac
Mikel Cortes <45786396+cortze@users.noreply.github.com>
2025-08-19 11:46:43 +0200

    pass full discv peer to dialer

--- eth/peer_dialer.go
+++ eth/peer_dialer.go
@@ -5,20 +5,29 @@ import (
 	"log/slog"
 	"time"
 
-	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/thejerf/suture/v4"
 
 	"github.com/probe-lab/hermes/host"
 )
+
+type peeringOptions struct {
+	// simple flag to check if we need to filter out the peers that we will connect
+	filter bool
+
+	attestnets uint64
+	syncnets   uint64
+}
 
 // PeerDialer is a suture service that reads peers from the peerChan (which
 // is filled by the [Discovery] service until that peerChan channel is closed.
 // When PeerDialer sees a new peer, it does a few sanity checks and tries
 // to establish a connection.
 type PeerDialer struct {
-	host     *host.Host
-	peerChan <-chan peer.AddrInfo
-	maxPeers int
+	peeringOpts peeringOptions
+	host        *host.Host
+	peerChan    <-chan *DiscoveredPeer
+	maxPeers    int
 }
 
 var _ suture.Service = (*PeerDialer)(nil)
@@ -30,6 +39,7 @@ func (p *PeerDialer) Serve(ctx context.Context) error {
 	for {
 		// if we're at capacity, don't look for more peers
 		if len(p.host.Network().Peers()) >= p.maxPeers {
+			// TODO: add check to see if we need to rotate any of the peers
 			select {
 			case <-ctx.Done():
 				return nil
@@ -40,27 +50,40 @@ func (p *PeerDialer) Serve(ctx context.Context) error {
 		}
 
 		var (
-			more     bool
-			addrInfo peer.AddrInfo
+			more    bool
+			newPeer *DiscoveredPeer
 		)
 		select {
 		case <-ctx.Done():
 			return nil
-		case addrInfo, more = <-p.peerChan:
+		case newPeer, more = <-p.peerChan:
 			if !more {
 				return nil
 			}
 		}
 
 		// don't connect with ourselves
-		if addrInfo.ID == p.host.ID() {
+		if newPeer.AddrInfo.ID == p.host.ID() {
 			continue
 		}
 
-		// finally, start the connection establishment.
-		// The success case is handled in net_notifiee.go.
-		timeoutCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
-		_ = p.host.Connect(timeoutCtx, addrInfo) // ignore error, this happens all the time
-		cancel()
+		// check if we are already connected to the peer
+		pConnect := p.host.Network().Connectedness(newPeer.AddrInfo.ID)
+		switch pConnect {
+		case network.NotConnected, network.CanConnect:
+			// filter to only peers that support attestation networks that we want
+			// - if we are not subscribed to subnets, connect any node
+
+			// finally, start the connection establishment.
+			// The success case is handled in net_notifiee.go.
+			timeoutCtx, cancel := context.WithTimeout(ctx, 2*time.Second)
+			_ = p.host.Connect(timeoutCtx, newPeer.AddrInfo) // ignore error, this happens all the time
+			cancel()
+
+		case network.Connected:
+			continue // the peer is already connected
+		default:
+			continue
+		}
 	}
 }
